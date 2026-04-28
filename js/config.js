bba60f074ad029f844fa6adce7f88dd6296eeb71
Raman Garg <ramangarg2007@gmail.com>
2026-04-27 23:29:58 -0500

    Update config.js
    updated client id

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'us-east-2_zu4k1YS87', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '3rum1ajh6tntdpso9aokkso9j5', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '4i8k6odccisidmahdcjaabr0k2g85ppca3tronconsn4klt47va', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-2' // e.g. us-east-2
     },
     api: {
